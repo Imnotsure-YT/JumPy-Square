7e58950b05679f94f97ced603f798f6349b011bd
Imnotsure-YT <148602739+Imnotsure-YT@users.noreply.github.com>
2024-03-13 20:31:26 -0700

    Delete collider.py

--- collider.py
+++
@@ -1,8 +0,0 @@
-# def isCollide(col, x, y):
-#     n = len(col)
-#     for i in range(n):
-#         for j in range(4):
-#             # col[i][j] = one point
-#             # col[i][j][0] is the x coord
-#             # col[i][j][1] is the y coord
-            
